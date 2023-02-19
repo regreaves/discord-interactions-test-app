818bf6e562cda09310b8b740d916561b3bb0ca1d
Ralph Greaves <rgreaves@ycp.edu>
2023-02-18 21:53:11 -0500

    Create index.mjs

---
+++ index.mjs
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+
+import express from 'express';
+
+import { router } from './routes/index.mjs';
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
